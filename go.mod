7976075abef2fe535d3888ac59820a7d2dae25ae
theoreotm <qwertzybad@gmail.com>
2024-01-20 21:12:51 +0500

    feat(route): Check primary db health

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.21.5
 require (
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
+
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,26 +1,46 @@
 package server
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", s.HelloWorldHandler)
+	r.Get("/database", s.PrimaryDatabaseHandler)
 
 	return r
 }
 
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) PrimaryDatabaseHandler(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
-	resp["message"] = "Hello World"
+
+	db, err := sql.Open("postgres", os.Getenv("PRIMARY_DB_URI"))
+	if err != nil {
+		resp["message"] = "Error connecting to database" + err.Error()
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatalf("error handling JSON marshal. Err: %v", err)
+		}
+		// Set status to 500
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write(jsonResp)
+		return
+	}
+	if db != nil {
+		db.Close()
+	}
+
+	resp["message"] = "Primary database connection successful"
 
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
